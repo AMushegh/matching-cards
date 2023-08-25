fb8405acaf47672388204a6340b733e16d01cbf0
Mushegh Andrasyan <mushegh.andrasyan@gmail.com>
2023-08-25 21:14:57 +0400

    adds webpack

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import "reflect-metadata";
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root")!);
+
+root.render(<h1>hello</h1>);
